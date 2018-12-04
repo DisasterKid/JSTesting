2f3b31d9adb8a2a7768edcd4141910e20d70eaa6
Jon <stamen37@gmail.com>
2018-12-04 20:07:49 +0800

    challenge four work

--- main.js
+++ main.js
@@ -98,33 +98,57 @@ findLongestWordLength("The quick brown fox jumped over the lazy dog");
 // fint the largest number inside the sub arrays of the array given
 
 function largestOfFour(arr) {
-    // You can do this!
-    var arrTwo = [[]];
+    var arrTwo = []; 
     // loop for moving through the outer array and nested array
     for (var i = 0; i < arr.length; i++)
     {
         
-        for (var j = 0; j < arr.length; j++)
+       //console.log(arr[i]);
+       //var num = 0 ;
+       //var fun = arr[i][i];
+       arrTwo = arr[i].filter(num => num > arr[i][i]);
+       console.log(arrTwo);
+       
+         for (var j = 0; j < arr.length; j++)
         {
-
            
-           console.log(arr[i][j]);
-           //console.log(arr[i][j]);
+          // arrTwo = arr[i][j];
+
+         // arrTwo = arr[i].filter(num => num > arr[i]);
+          //console.log(arrTwo);
            
-            /* if (arr[i][i] > arr[i][j])
-            {
-                
-               // console.log(arr[i][i]);
-                console.log(arr[i][j]);
-                arrTwo[i] = arr[i][j];
-            }*/
-
-
-        }
+        } 
         
-    }
+    } 
+
+
+    
+
+   
     //console.log(arrTwo);
     return arr;
   }
   
   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+  
+
+
+
+
+ /* var arr = [[1,2,3,4],[1,2,3,4]];
+
+  var flat = arr.reduce(function (accumulator, currentValue)
+  
+  
+  {
+      return accumulator.concat(currentValue);
+  },
+  []
+  ); 
+  
+  
+  var testFilter = flat.filter(num => num > 2);
+
+  console.log(testFilter);  */
